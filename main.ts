5548af968c8ec889485912ff4d0de5405fcf5fa9
Dirkiedee <74057480+Dirkiedee@users.noreply.github.com>
2020-11-06 14:39:46 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Dollar), function () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
 })
